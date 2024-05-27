f402b9015200334e7dabfdd4f2a5b31f0a0a36b2
Cristian Monzon <cristian-02-@live.com>
2024-05-27 13:42:22 -0500

    Agregando la estructura de carpetas

---
+++ backend/src/main/java/com/fedario_digital/fedatario_digital/FedatarioDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.fedario_digital.fedatario_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedatarioDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedatarioDigitalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fedario_digital/fedatario_digital/FedatarioDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedario_digital.fedatario_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedatarioDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
